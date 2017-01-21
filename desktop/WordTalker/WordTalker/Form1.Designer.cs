fbb11afac538044b9d3ecb42c4efeb0a132586ab
Touhid Mia <hmtm.cse@gmail.com>
2017-01-21 22:38:59 +0600

    Destop App

---
+++ desktop/WordTalker/WordTalker/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WordTalker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.englishWord = new System.Windows.Forms.Label();
+            this.banglaMeaning = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Location = new System.Drawing.Point(1, 154);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1211, 347);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Word Notes";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(6, 19);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(715, 307);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Talking";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // englishWord
+            // 
+            this.englishWord.AutoSize = true;
+            this.englishWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.englishWord.Location = new System.Drawing.Point(34, 94);
+            this.englishWord.Name = "englishWord";
+            this.englishWord.Size = new System.Drawing.Size(258, 46);
+            this.englishWord.TabIndex = 3;
+            this.englishWord.Text = "English Word";
+            // 
+            // banglaMeaning
+            // 
+            this.banglaMeaning.AutoSize = true;
+            this.banglaMeaning.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.banglaMeaning.Location = new System.Drawing.Point(464, 94);
+            this.banglaMeaning.Name = "banglaMeaning";
+            this.banglaMeaning.Size = new System.Drawing.Size(307, 46);
+            this.banglaMeaning.TabIndex = 4;
+            this.banglaMeaning.Text = "Bangla Meaning";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(287, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 513);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.banglaMeaning);
+            this.Controls.Add(this.englishWord);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Word Talker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label englishWord;
+        private System.Windows.Forms.Label banglaMeaning;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ desktop/WordTalker/WordTalker/Form1.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordTalker
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        List<Dictionary<string, string>> words = new List<Dictionary<string, string>>();
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        Thread thread;
+        int lastIndex = 0;
+        Boolean isStartTalking = false;
+        int sleepTime = 0;
+
+       
+        
+
+        private string pullValue(string key,int index)
+        {
+            Dictionary<string, string> data = words[index];
+            return data[key];
+        }
+
+
+        private void loadData()
+        {
+            if(words.Count() == 0)
+            {
+                using (TextFieldParser parser = new TextFieldParser(@"ielts_official_1_8.csv"))
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+                    listView1.View = View.Details;
+                    listView1.GridLines = true;
+                    listView1.FullRowSelect = true;
+                    listView1.Columns.Add("SL", 100);
+                    listView1.Columns.Add("English Word", 100);
+                    listView1.Columns.Add("Bangla Meaning", 100);
+                    listView1.Columns.Add("Parts of Speech", 100);
+                    listView1.Columns.Add("Synonym", 100);
+                    Dictionary<string, string> row = new Dictionary<string, string>();
+                    int numberOfWord = 0;
+                    while (!parser.EndOfData)
+                    {
+                        row = new Dictionary<string, string>();
+                        string[] tmp = parser.ReadFields();
+                        string[] fields = new string[tmp.Length + 1];
+                        fields[0] = numberOfWord + "";
+                        tmp.CopyTo(fields, 1);
+
+                        row.Add("english", fields[1]);
+                        row.Add("bangla", fields[2]);
+                        words.Add(row);
+                        listView1.Items.Add(new ListViewItem(fields));
+                        numberOfWord++;
+                    }
+                }
+
+            }
+
+        }
+
+        delegate void SetTextCallback(List<Dictionary<string, string>> words, int index);
+        private void SetText(List<Dictionary<string, string>> words, int index)
+        {
+            if (this.englishWord.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { words, index });
+            }
+            else
+            {
+                this.englishWord.Text = pullValue("english", index);
+                lastIndex = index;
+            }
+
+            if (this.banglaMeaning.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { words, index });
+            }
+            else
+            {
+                this.banglaMeaning.Text = pullValue("bangla", index);
+                lastIndex = index;
+            }
+
+
+            if (this.listView1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { words, index });
+            }
+            else
+            {
+                if (index != 0)
+                {
+                    listView1.Items[index - 1].Selected = false;
+                }
+                listView1.Items[index].Selected = true;
+                listView1.Items[index].Focused = true;
+                listView1.TopItem = listView1.Items[index];
+                listView1.Select();
+
+            }
+        }
+
+        public void updateLabel()
+        {
+            synth.SetOutputToDefaultAudioDevice();
+            for (int i = lastIndex; i < words.Count(); i++)
+            {
+                this.SetText(words, i);
+                synth.Speak(pullValue("english",i));
+                Thread.Sleep(1000 * sleepTime);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loadData();
+            if (isStartTalking)
+            {
+                isStartTalking = false;
+                button1.Text = "Start Talking";
+                thread.Abort();
+                comboBox1.Enabled = true;
+            }
+            else
+            {
+                thread = new Thread(updateLabel);
+                thread.Start();
+                isStartTalking = true;
+                button1.Text = "Stop Talking";
+                comboBox1.Enabled = false;
+            }
+          
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sleepTime = int.Parse(comboBox1.SelectedItem.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}

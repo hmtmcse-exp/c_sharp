ef0311eee9a0ae539be0f9c783288f8505d37683
Touhid Mia <touhid@bitmascot.com>
2017-05-14 17:15:58 +0600

    Touhid

---
+++ console/UIBrowser/UIBrowser/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog x = new OpenFileDialog();
+
+            x.DefaultExt = "ttf";
+            x.Filter = "ttf files (*.ttf)|*.ttf";
+            x.Multiselect = true;
+            x.ShowDialog();
+            string[] result = x.FileNames;
+
+            foreach (string y in result)
+                MessageBox.Show(y, "Selected Item", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}

2067dffd8c00745013ee2321365177ccef5097b4
Touhid Mia <hmtm.cse@gmail.com>
2017-06-06 05:08:19 +0600

    regex

---
+++ console/RegexExamplePro/RegexExamplePro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RegexExamplePro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            var location = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "RegexExamplePro");
+            var textLoc = Path.Combine(location, "AppDependencyInjector.txt");
+            var text = File.ReadAllText(textLoc);
+            Console.WriteLine(text);
+            
+            string newString = Regex.Replace("abcb", "b", "<td>${0}</td>");
+            Console.WriteLine(newString);
+
+        }
+    }
+}
--- desktop/ScaffoldGenerator/ScaffoldGenerator/DotnetCore/FormInpuGenerator.cs
+++ desktop/ScaffoldGenerator/ScaffoldGenerator/DotnetCore/FormInpuGenerator.cs
@@ -8,5 +8,6 @@ namespace ScaffoldGenerator.DotnetCore
 {
     class FormInpuGenerator
     {
+
     }
 }
